c64d62ccafe581dc8e80d846ee3fcc1bae38be80
Ryuta Hamasaki <ryuta.hamasaki@accredify.io>
2022-12-21 18:41:40 +0800

    add sanctum middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'abilities' => \Laravel\Sanctum\Http\Middleware\CheckAbilities::class,
+        'ability' => \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
